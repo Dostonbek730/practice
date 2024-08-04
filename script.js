b08315c8380ddcb0e0aaac1a093714ed64aa6857
Doston <abdumanabovdoston@gmail.com>
2024-08-04 17:25:11 +0500

    first project

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberOfMovies = prompt('Сколько фильмов вы посмотрели? ', '')
+
+const personalMovieDB = {
+    count: numberOfMovies,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const   a = prompt('Один из последних просмотреных фильмов? '),
+        b = prompt('На сколько оцените его? '),
+        c = prompt('Один из последних просмотреных фильмов? '),
+        d = prompt('На сколько оцените его? ');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
