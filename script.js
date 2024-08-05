b845911767ed868f6519efd762982052ccf0a013
Doston <abdumanabovdoston@gmail.com>
2024-08-05 18:51:53 +0500

    practice 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict';
 /* 
 const numberOfMovies = prompt('Сколько фильмов вы посмотрели? ', '')
 
@@ -18,4 +19,40 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB) 
+
+
+
+let nameOfMovie = prompt('Один из последних просмотреных фильмов? ', '');
+
+if (nameOfMovie.length < 10){
+    let Movies
+    do {
+        Movies = prompt('Ответ не верный. Один из последних просмотреных фильмов? ', '')
+    }
+    while (Movies.length < 10);
+}
+
+
+let numberOfMovies = prompt('Сколько фильмов вы посмотрели? ', '');
+
+const personalMovieDB = {
+    count: numberOfMovies,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+if (personalMovieDB.count < 10 && 0 < personalMovieDB.count){
+    alert('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count > 10 && 30 > personalMovieDB.count) {
+    alert('Вы классический зритель')
+} else if (personalMovieDB.count > 30){
+    alert('Вы киноман')
+} else if (personalMovieDB.count < 0){
+    alert('Произошла ошибка')
+} else {
+    alert('Произошла ошибка')
+}
+
 */
